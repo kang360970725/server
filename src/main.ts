fac89131385b8b47b8cc34a3267c0822d868cb68
chenkang <chenkang@zhiketong.cn>
2025-12-31 00:15:15 +0800

    *) 权限调整

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const port = Number(process.env.PORT || 3000);
+  const port = Number(process.env.PORT || 80);
   await app.listen(port, '0.0.0.0');
 }
 bootstrap();
