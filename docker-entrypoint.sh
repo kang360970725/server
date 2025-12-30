f685ade448b2155c5d0232222303016510c2bc49
chenkang <chenkang@zhiketong.cn>
2025-12-31 01:51:44 +0800

    *) 权限调整

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,15 +1,5 @@
 #!/bin/sh
 set -e
 
-echo "Starting with NODE_ENV=$NODE_ENV"
-
-echo "Running prisma db push..."
-npx prisma db push
-
-if [ "$RUN_SEED" = "1" ]; then
-  echo "Running prisma seed..."
-  npx prisma db seed
-fi
-
 echo "Starting app..."
-node dist/main
+node dist/main.js
