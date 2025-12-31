b67a252c5f1ce8cd4d4d5797e367462e23ecfac4
chenkang <chenkang@zhiketong.cn>
2025-12-31 18:50:08 +0800

    *) 发布脚本调整

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,16 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = Number(process.env.PORT || 80);
+  app.enableCors({
+    origin: [
+      'http://pc.welax-tech.com',
+      'https://pc.welax-tech.com', // 以后有 https 直接可用
+      'http://localhost:8000',
+      'http://localhost:8001',
+    ],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
   await app.listen(port, '0.0.0.0');
 }
 bootstrap();
