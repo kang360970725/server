8998b1357147994fa1a7d69371404c045f784c89
chenkang <chenkang@zhiketong.cn>
2026-06-06 23:34:50 +0800

    *）发布报错问题提交

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -2770,6 +2770,7 @@ export class OrdersService {
                                         userType: true,
                                         staffRating: {
                                             select: {
+                                                name: true,
                                                 rate: true,
                                             },
                                         },
